4b9e360542f214e7c75854b94c480a637b76e30d
Freddie Butterfield <fbutterfield1@sheffield.ac.uk>
2023-01-21 13:21:46 +0000

    Added txt files

---
+++ program/options.py
@@ -0,0 +1,9 @@
+# The original idea here was to scrape the Tesco website,
+# however due to problems with getting requests I have gone
+# for a hard coded approach by reading in options from a text
+# file.
+
+
+mains = []
+snacks = []
+drinks = []
--- soupstuff/souptesting.py
+++ soupstuff/souptesting.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
+import webbrowser
 
 url = "https://www.bbc.co.uk/"
+url1 = "https://www.tesco.com/groceries/en-GB/shop/fresh-food/chilled-soup-sandwiches-and-salad-pots/pound3.90-meal-deal?page=1"
 req = requests.get(url)
 
 #soup = BeautifulSoup("<html>a web page</html>", 'html.parser')
@@ -18,3 +20,5 @@ for string in head.stripped_strings:
 
 for string in body.stripped_strings:
     print(string)
+
+webbrowser.open(url1)
