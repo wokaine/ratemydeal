971653ccdd254ceef504d0650095f61cb9064aca
Freddie Butterfield <fbutterfield1@sheffield.ac.uk>
2023-01-21 16:39:30 +0000

    created loading script

--- program/options.py
+++ program/options.py
@@ -3,7 +3,23 @@
 # for a hard coded approach by reading in options from a text
 # file.
 
+def getOptions():
+    mains = []
+    snacks = []
+    drinks = []
+    print("Importing mains...")
+    with open('data/mains.txt', 'r') as file:
+        mains = [line.rstrip() for line in file.readlines()]
+    print("Done!")
 
-mains = []
-snacks = []
-drinks = []
+    print("\nImporting snacks...")
+    with open('data/snacks.txt', 'r') as file:
+        snacks = [line.rstrip() for line in file.readlines()]
+    print("Done!")
+
+    print("\nImporting drinks...")
+    with open('data/drinks.txt', 'r') as file:
+        drinks = [line.rstrip() for line in file.readlines()]
+    print("Done!")
+
+    return mains, snacks, drinks
