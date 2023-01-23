593ff7d778e2bbccbdf8394309b1af57994be20e
Freddie Butterfield <fbutterfield1@sheffield.ac.uk>
2023-01-23 12:07:36 +0000

    project layout/cleanup

--- flaskstuff/flasktesting.py
+++ flaskstuff/flasktesting.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, url_for
+from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello World</p>"
+
+# @app.route("/<name>")
+# def hello(name):
+#     return f"Hello, {escape(name)}"
+
+# @app.route('/user/<username>')
+# def show_user_profile(username):
+#     # show the user profile for that user
+#     return f'User {escape(username)}'
+
+# @app.route('/post/<int:post_id>')
+# def show_post(post_id):
+#     # show the post with the given id, the id is an integer
+#     return f'Post {post_id}'
+
+# @app.route('/path/<path:subpath>')
+# def show_subpath(subpath):
+#     # show the subpath after /path/
+#     return f'Subpath {escape(subpath)}'
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
 
 if __name__ == '__main__':
     app.run()
