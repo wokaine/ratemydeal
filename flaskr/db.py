e356df0081da288fd79b0b821bc2784d684e8a7a
Freddie Butterfield <fbutterfield1@sheffield.ac.uk>
2023-01-31 11:41:14 +0000

    Added env variables

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,10 +1,15 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+import os
+
+username = os.environ.get("MONGO_USERNAME")
+password = os.environ.get("MONGO_PASSWORD")
 
 print("Connecting to database...")
 try:
+    print(f"mongodb+srv://{username}:{password}@cluster0.u1zrqbz.mongodb.net/?retryWrites=true&w=majority")
     client = MongoClient(
-    "mongodb+srv://ratemydeal1:tS0gr4i7Z5sBtos3@cluster0.u1zrqbz.mongodb.net/?retryWrites=true&w=majority", 
+    f"mongodb+srv://{username}:{password}@cluster0.u1zrqbz.mongodb.net/?retryWrites=true&w=majority", 
     server_api=ServerApi('1'))
     db = client.get_database('meal_deals')
     print("...Connected")
